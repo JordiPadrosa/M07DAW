9ff71a158c7775d2d1ecc88c0ba2326a1fd34b56
JordiPadrosa <jpadrosa8@boscdelacoma.cat>
2022-09-19 16:08:09 +0200

    tot fet

---
+++ encrypt.php
@@ -0,0 +1,18 @@
+<?php
+$clave  = 'jordi';
+$method = 'aes-256-cbc';
+$iv = base64_decode("C9fBxl1EWtYTL1/M8jfstw==");
+ $encriptar = function ($text) use ($metode, $clau, $iv) {
+     return openssl_encrypt ($text, $metode, $clau, false, $iv);
+ };
+ 
+ $desencriptar = function ($text) use ($metode, $clau, $iv) {
+     $text_encriptat = base64_decode($text);
+     return openssl_decrypt($text, $metode, $clau, false, $iv);
+ };
+ 
+$text = "Hola sóc en Jordi";
+$text_encriptat = $encriptar($text);
+$text_desencriptat = $desencriptar($text_encriptat);
+echo 'Encriptat: '. $text_encriptat . '<br>';
+echo 'Desencriptat: '. $text_desencriptat . '<br>';
--- variables_primitius.php
+++ variables_primitius.php
@@ -1,7 +1,22 @@
 <?php
 $i = 12;
+$a = 2.5;
+$b = true;
+$c = "jordi";
 $tipus_de_i = gettype( $i );
 echo "La variable \$i 
       conté el valor $i 
-	  i és del tipus $tipus_de_i";
+	  i és del tipus $tipus_de_i<br>";
+$tipus_de_a = gettype( $a );
+echo "La variable \$a 
+      conté el valor $a 
+	  i és del tipus $tipus_de_a<br>";
+$tipus_de_b = gettype( $b );
+echo "La variable \$b 
+      conté el valor $b 
+	  i és del tipus $tipus_de_i<br>";
+$tipus_de_c = gettype( $c );
+echo "La variable \$c
+      conté el valor $c 
+	  i és del tipus $tipus_de_c";
 ?>
