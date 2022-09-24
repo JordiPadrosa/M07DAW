7ab5b4f3ab645406777f59bf907fd97aa44ffbf7
JordiPadrosa <jpadrosa8@boscdelacoma.cat>
2022-09-24 19:30:11 +0200

    tot fet

--- Arrays i funcions/matriu.php
+++ Arrays i funcions/matriu.php
@@ -1,14 +1,47 @@
 <?php
 $n = 4;
-echo creaMatriu();
+$taula = creaMatriu($n);
+echo mostraMatriu($taula, $n);
+echo transposaMatriu($taula, $n);
 function creaMatriu($n){
     for($i=0;$i<$n;$i++){
-        echo "<table>";
-        echo "<tr>";
-        echo "  <th> Nombre  </th>";
-        echo "</tr>";
-
+        for($y=0;$y<$n;$y++){
+            if($y==$i){
+                $taula[$i][$y] = "*";
+            }elseif($i>$y){
+                $taula[$i][$y] = rand(10,20);
+            }else{
+                $taula[$i][$y] = $i+$y;
+            }
+        }
     }
+    return $taula;
+}
+function mostraMatriu($taula, $n){
+    $string = "<table>";
+    for ($i=0;$i<$n;$i++) {
+        $string = $string."<tr>";
+        for ($y=0;$y<$n;$y++) {
+                $string = $string."<td>".$taula[$i][$y]."</td>";
+        }
+        $string = $string."</tr>";
+    }
+    $string = $string."</table><br>";
+    return $string;
+}
+function transposaMatriu($taula, $n){
+    for ($i=0;$i<$n;$i++){
+        for ($y=0;$y<$n;$y++){
+            if($y==$i){
+                $taulaGirada[$i][$y] = "*";
+            }elseif($i>$y){
+                $taulaGirada[$i][$y] = $taula[$y][$i];
+            }else{
+                $taulaGirada[$i][$y] = $taula[$y][$i];
+            }
+        }
+    }
+    return mostraMatriu($taulaGirada,$n);
 }
 
 ?>
