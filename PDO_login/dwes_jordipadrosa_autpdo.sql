84c0659b6632a135cf5310b244bb709b9ae6e06e
JordiPadrosa <102662355+JordiPadrosa@users.noreply.github.com>
2022-11-11 19:51:54 +0100

    Update dwes_jordipadrosa_autpdo.sql

--- PDO_login/dwes_jordipadrosa_autpdo.sql
+++ PDO_login/dwes_jordipadrosa_autpdo.sql
@@ -4,4 +4,4 @@ GRANT ALL PRIVILEGES ON dwes_jordipadrosa_autpdo.* TO 'dwes-user'@'%';
 
 use `dwes_jordipadrosa_autpdo`;
 create table users ( `email` varchar(30) primary key, `password` varchar(32),  `name` varchar(20));
-create table connexions ( id int primary key auto_increment, ip varchar(15), `user` varchar(30), `time` varchar(30), `status` varchar(20));
+create table connexions ( id int primary key auto_increment, ip varchar(15), `user` varchar(30), `time` varchar(30), `status` varchar(30));
